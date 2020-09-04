4e4bab1dfca7dcde329ac3ea0db53f69d9a7dcaa
Ali Alam <contact@aliserajalam.com>
2020-09-04 12:20:27 +1000

    Updates

---
+++ precache-manifest.9ac9345a1f23d29291c119242f2bd0bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a8cf80104a5e06680e1bfd6485851d",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "78d9692573ff1743eb70",
+    "url": "/kanban/static/css/main.17dae216.chunk.css"
+  },
+  {
+    "revision": "a3cffe91562476a9a6d2",
+    "url": "/kanban/static/js/2.ef4199f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kanban/static/js/2.ef4199f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d9692573ff1743eb70",
+    "url": "/kanban/static/js/main.c74d39da.chunk.js"
+  },
+  {
+    "revision": "f5aa037a105c127e43db",
+    "url": "/kanban/static/js/runtime-main.72bc9640.js"
+  }
+]);
